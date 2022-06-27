a43fab3f145e223923a055dd46a821443e5ce2c8
코너C <3116620+chany@users.noreply.github.com>
2022-06-27 10:20:03 +0900

    Update build.rs

--- gRPC/Tutorial01/build.rs
+++ gRPC/Tutorial01/build.rs
@@ -1,6 +1,6 @@
 // https://www.youtube.com/watch?v=JkSa-qA2jnY
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("/home/chan/OnDev/gRPC_tutorial/src/proto/payments.proto")?;
+    tonic_build::compile_protos("./src/proto/payments.proto")?;
     Ok(())
 }
